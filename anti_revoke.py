78e814029d7c88658709868b465b3c358c4ee1d4
Thomas WANG <554244174@qq.com>
2020-08-14 00:37:21 +0800

    make structure

---
+++ anti_revoke.py
@@ -0,0 +1,9 @@
+
+module_anti_revoke_status = 0
+cbyl_init_name = "北京的咕咕咕咕菇"
+
+def module_anti_revoke_init():
+    module_anti_revoke_status = 1
+
+def anti_revoke_note_msg_process(bot, msg, old_msg):
+    pass
---
+++ keyword_award.py
@@ -0,0 +1,8 @@
+
+module_keyword_award_status = 0
+
+def module_keyword_award_init():
+    module_keyword_award_status = 1
+
+def keyword_award_normal_msg_process(bot, msg):
+    pass
